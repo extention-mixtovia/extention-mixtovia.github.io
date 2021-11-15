2c6d315375950f4f20b6f046a32076b591e40664
extention-mixtovia <94383951+extention-mixtovia@users.noreply.github.com>
2021-11-15 17:41:46 +0100

    Update script.js

--- extention-files/script.js
+++ extention-files/script.js
@@ -4,3 +4,8 @@ btnPopup.addEventListener('click',openMoadl);
 function openMoadl() {
 overlay.style.display='block';
 }
+var btnClose = document.getElementById('btnClose');:Récupérer l'id de l'élément btnClose.
+btnClose.addEventListener('click',closeModal);:récupèrer l'évenement clic et déclenche la fonction closeModal
+function closeModal() {
+overlay.style.display='none';
+}
