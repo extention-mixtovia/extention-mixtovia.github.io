e2d4897cf448c38ec6a8d891525bdb445026256a
extention-mixtovia <94383951+extention-mixtovia@users.noreply.github.com>
2021-11-17 17:57:29 +0100

    Create videos_titles.js

---
+++ extention-files/videos_titles.js
@@ -0,0 +1,6 @@
+var v1n = Bonjour
+
+
+document.getElementById("video1name").innerHTML=v1n;
+document.getElementById("video2name").innerHTML=v2n;
+document.getElementById("video3name").innerHTML=v3n;
